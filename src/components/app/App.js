97e0e70bbe9aff4ce22c2ab34c3eb032b44d34e1
Sagi <33280055+sagis148@users.noreply.github.com>
2022-01-31 15:30:18 +0200

    Update App.js

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { backgroundImage } from '../../config.json';
-import flagQuestion, {  questions } from '../../flagQuestions.json';
+import question, {  questions } from '../../questions.json';
 import Quiz from '../quiz/Quiz';
 import './App.css';
 import axios from "axios";
@@ -8,7 +8,6 @@ import axios from "axios";
 class App extends React.Component {
 
   constructor(props) {
-    console.log("constructor app")
     super(props);
     this.state = {
       page: -1,//Current question page.
@@ -17,17 +16,17 @@ class App extends React.Component {
       qNum: 0,//Current question number.
       score: 0,//Current player score.
       playerName:"s",
-      difficulty:"3",
-      amountOfQuestions:"5",
-      gameQuestionsType:"country",
-
-      // isActive: false,
-      secondsElapsed: 45 //time in seconds
-
+      difficulty:"3",//Default - easy.
+      amountOfQuestions:"5",//Default - 5 questions.
+      gameQuestionsType:"country",//Default - flags questions.
+      secondsElapsed: 45 //Default - 45 seconds.
     }
   }
 
-//Get the current second.
+  /**
+   * Get the current time.
+   * @returns {string} The time in seconds.
+   */
   getSeconds() {
     if (this.secondsElapsed  >= 0) {
       return (("" + (this.secondsElapsed )).slice(-2));
@@ -36,64 +35,68 @@ class App extends React.Component {
       return "0"
     }
   }
-  //Start the countdown timer.
+  /**
+   * Start the countdown timer.
+   */
   startTime = () => {
-    // this.setState({ isActive: true });
-
     this.countdown = setInterval(() => {
       this.setState(({ secondsElapsed }) => ({
         secondsElapsed: secondsElapsed - 1
       }));
     }, 1000);
   };
-//Reset the countdown timer.
+  /**
+   * Reset the countdown timer.
+   */
   resetTime = () => {
     clearInterval(this.countdown);
-    this.setState({
-      secondsElapsed: 45,
-      // isActive: false
-    });
+    this.setState({secondsElapsed: 45,});
   };
-//Pause the countdown timer.
-  pauseTime = () => {
-    // console.log("secondsElapsed: ",this.state.secondsElapsed % 60)
-    clearInterval(this.countdown);
-    // this.setState({ isActive: false });
-  };
-
+  /**
+   * Pause the countdown timer.
+   */
+  pauseTime = () => {clearInterval(this.countdown)};
+  /**
+   * Sets the player name.
+   * @param e The name input.
+   */
   setPlayerName = (e) => {
-    this.setState({
-      playerName: e.target.value
-    }) ;
+    this.setState({playerName: e.target.value}) ;
   }
-  handleDifficultyChange=(e)=> {
-    this.setState({
-      difficulty : e.target.value,
-
-    })
-
+  /**
+   * Sets the difficulty.
+   * @param e The chosen difficulty.
+   */
+  setDifficultyChange=(e)=> {
+    this.setState({difficulty : e.target.value})
   }
-  handleQuestionsAmountChange=(e)=> {
-    this.setState({
-      amountOfQuestions : e.target.value
-    })
+  /**
+   * Sets the questions amount.
+   * @param e The chosen questions amount.
+   */
+  setQuestionsAmountChange=(e)=> {
+    this.setState({amountOfQuestions : e.target.value})
   }
-  handleTypeChange=(e)=> {
-    this.setState({
-      gameQuestionsType:e.target.value
-    })
+  /**
+   * Sets the question type.
+   * @param e The chosen question type.
+   */
+  setTypeChange=(e)=> {
+    this.setState({gameQuestionsType:e.target.value})
   }
+  /**
+   * Moves to the entry menu after name submission.
+   */
   handleNameSubmission = () => {
-    console.log("handleNameSubmission app")
-    this.setState({
-      page: this.state.page + 1
-    })
+    this.setState({page: this.state.page + 1})
   }
+  /**
+   * Resets the timer and randomly selects a random question.
+   */
   handlePageNext = () => {
     this.resetTime()
-    console.log("handlePageNext app")
     let min = 1;
-    let max = Number(flagQuestion.questions.length)//Maximum questions amount.
+    let max = Number(question.questions.length)//Maximum questions amount.
     if (this.state.qNum < Number (this.state.amountOfQuestions )+1) {
       this.setState({
         page: Number(Math.floor(Math.random() * (max - min) + min)),
@@ -101,12 +104,16 @@ class App extends React.Component {
       })
     }
   }
-
+  /**
+   * This function calculate the score, Summarized it and counts the amount of correct and incorrect answers .
+   * @param validAnswer True for correct answer, False for incorrect answer.
+   * @param clueButtonPenalty If the user press the hint button it value will be 0.5 otherwise its 1.
+   * @param currentTime The current time left.
+   */
   handleAnswerSubmission = (validAnswer,clueButtonPenalty,currentTime) => {
-    console.log("handleAnswerSubmission app");
-    // A correct answer score is calculated based on the difficulty of the game
-    // and the time it took for the user to answer the question and will be rounded to have a round score.
-    // If the hint was used the score would be cut in half.
+    // A correct answer score is calculated based on the difficulty of the game,
+    // the time it took for the user to answer the question and will be rounded to have a round score.
+    // If the hint button was used the score would be cut in half.
     let currentScore = Math.round(((10 * this.state.difficulty / 2 ) + currentTime) * clueButtonPenalty)
     if (validAnswer){
       this.setState((prevState) => ({
@@ -116,34 +123,41 @@ class App extends React.Component {
     }
     else this.setState({ numIncorrectAnswers: this.state.numIncorrectAnswers + 1 })
   }
-
+  /**
+   * Start over button pressed.
+   * Returns the user to the entry menu.
+   */
   reset = () => {
-    console.log("reset app")
     this.setState({
       page: 0,
       numCorrectAnswers: 0,
       numIncorrectAnswers: 0,
       qNum:0,
-      score:0,
+      score:0
     });
   }
-
+  /**
+   * End game button pressed.
+   * Returns the user to the main menu.
+   */
   endGame = () => {
-    console.log("endGame app")
     this.setState({
       page: -1,
       numCorrectAnswers: 0,
       numIncorrectAnswers: 0,
       qNum:0,
-      score:0
+      score:0,
+      difficulty:"3",
+      amountOfQuestions:"5",
+      gameQuestionsType:"country"
     });
-    axios.post('http://localhost:2000/logOut', {})
+    axios.post('http://localhost:2000/logOut')
   }
 
   render() {
-    // console.log("render app")
     return (
         <div className="App" style={{ backgroundImage: `url('${window.location.origin}/${backgroundImage}')`}}>
+
           <Quiz
               page={this.state.page}
               qNum={this.state.qNum}
@@ -156,9 +170,9 @@ class App extends React.Component {
               numIncorrectAnswers={this.state.numIncorrectAnswers}
               handleAnswer={this.handleAnswer}
               setPlayerName={this.setPlayerName}
-              handleDifficultyChange={this.handleDifficultyChange}
-              handleQuestionsAmountChange={this.handleQuestionsAmountChange}
-              handleTypeChange={this.handleTypeChange}
+              setDifficultyChange={this.setDifficultyChange}
+              setQuestionsAmountChange={this.setQuestionsAmountChange}
+              setTypeChange={this.setTypeChange}
               handlePageNext={this.handlePageNext}
               handleAnswerSubmission={this.handleAnswerSubmission}
               reset={this.reset}
@@ -166,16 +180,13 @@ class App extends React.Component {
               randomAnswers = {this.createRandomAnswers}
               handleNameSubmission={this.handleNameSubmission}
               questions={questions}
-
-              // isActive={this.state.isActive}
               secondsElapsed={this.state.secondsElapsed}
-
               startTime={this.startTime}
               resetTime={this.resetTime}
               pauseTime={this.pauseTime}
               getSeconds={this.getSeconds}
-
           />
+
         </div>
     );
   }
