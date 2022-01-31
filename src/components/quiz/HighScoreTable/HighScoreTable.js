3d0bf4c04343013deae94651f1adf4620ea7cd21
Sagi <33280055+sagis148@users.noreply.github.com>
2022-01-31 15:30:57 +0200

    Update HighScoreTable.js

--- src/components/quiz/HighScoreTable/HighScoreTable.js
+++ src/components/quiz/HighScoreTable/HighScoreTable.js
@@ -1,15 +1,12 @@
 import React from 'react';
 
 class HighScoreTable extends React.Component {
-
     render() {
         return (
             <tr>
                 <td> </td>
                 <td> {this.props.obj.username} </td>
-                {/*<td>*/}
-                {/*    {this.props.obj.loggedIn.toString()}*/}
-                {/*</td>*/}
+                {/*<td>{this.props.obj.loggedIn.toString()}</td>*/}
                 <td> {this.props.obj.score} </td>
                 <td> {this.props.obj.date} </td>
             </tr>
